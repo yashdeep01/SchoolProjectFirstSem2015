9417b07d9c3a4da1fa9d229bf1c9a8659579ec52
nukex1122 <md.mohsin.hussain786@gmail.com>
2015-08-15 12:30:46 +0530

    Some Menu Changes READ DESCRIPTION
    OYE make the billing system. Bills Of Diff Order Will be in Different
    Files.

--- Main.cpp
+++ Main.cpp
@@ -19,7 +19,7 @@ class order{
 };
 
 class Item{
-	char name[10];
+	char name[100];
 	int price;
 	public:
 
@@ -47,7 +47,7 @@ class Item{
 
 void foodMenu();
 void mainMenu();
-
+void orderMenu();
 
 void main(){
 	clrscr();
@@ -66,37 +66,62 @@ void mainMenu(){
 			foodMenu();
 		}
 	}
-
 }
 
 void foodMenu(){
-	cout<<"1)Show Menu \n2)Add Items";
-	int c;
-	cin>>c;
-	if(c==1){
+	int c=1;
+	while(c<4){
+		cout<<"1)Show Menu \n2)Add Items \n3)Order Items";
+		cin>>c;
+		if(c==1){
+			fstream f,g;
+			char sent[100],price[100];
+			f.open("foodMenu.txt",ios::in);
+			g.open("price.txt",ios::in);
+			while(!f.eof()){
+				f.getline(sent,100,'\n');
+				g.getline(price,100,'\n');
+
+				cout<<sent<<"  "<<price<<endl;
+			}
+			g.close();
+			f.close();
+		}
+		if (c==2){
+			Item d;
+			fstream itemName,price;
+			itemName.open("foodMenu.txt",ios::app);
+			price.open("price.txt",ios::app);
+			d.input();//COMPLETE IT
+			itemName<<d.getName()<<endl;
+			price<<d.getPrice()<<endl;
+			itemName.close();
+			price.close();
+		}
+		if (c==3)
+		{
+			orderMenu();
+		}
+	}
+}
+
+void orderMenu(){
+	int j=1;
+	while(j!=9){
+		clrscr();
+		cout<<"Menu\n";
 		fstream f,g;
-		Data d;
-		char sent[100],price[10];
+		char sent[100],price[100];
 		f.open("foodMenu.txt",ios::in);
 		g.open("price.txt",ios::in);
 		while(!f.eof()){
 			f.getline(sent,100,'\n');
 			g.getline(price,100,'\n');
-
-			cout<<sent<<"  "<<price<<endl;
+				cout<<sent<<"  "<<price<<endl;
 		}
 		g.close();
 		f.close();
-	}
-	if (c==2){
-		Item d;
-		fstream itemName,price;
-		itemName.open("foodMenu.txt",ios::app);
-		price.open("price.txt",ios::app);
-		d.input();//COMPLETE IT
-		itemName<<d.getName()<<endl;
-		price<<d.getPrice()<<endl;
-		itemName.close();
-		price.close();
+		cout<<"add corresponing items by number\npress x to exit\n";
+		cin>>j;
 	}
 }
